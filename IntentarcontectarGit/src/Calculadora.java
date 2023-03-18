7833b3436edeeef7023a9b22be61ab40840117ca
fito_ <fito_@Fito>
2023-03-17 22:53:58 -0300

    Inicio

---
+++ IntentarcontectarGit/src/Calculadora.java
@@ -0,0 +1,26 @@
+
+public class Calculadora {
+
+	public static int sumar(int valor1, int valor2) {
+		int resultadoSuma= valor1+=valor2;
+		System.out.println(resultadoSuma);
+		return resultadoSuma;
+
+	}
+	public static int multiplicar(int valor1, int valor2) {
+		int resultadoMulti=valor1*=valor2;
+		System.out.println(resultadoMulti);
+		return resultadoMulti;
+
+	}
+	public static int dividir(int valor1,int valor2) {
+		int resultadoDivi=valor1/=valor2;
+		System.out.println(resultadoDivi);
+		return resultadoDivi;
+
+	}
+
+	
+	
+	
+}
---
+++ IntentarcontectarGit/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 1;
+		
+	Calculadora.sumar(a, b);
+	Calculadora.multiplicar(a, a);
+	
+
+	
+		
+	}
+
+}
